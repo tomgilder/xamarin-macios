a58d8b8ed31d89ccee514ba2d4f59ce7434ae130
Jeffrey Stedfast <jestedfa@microsoft.com>
2016-12-14 12:53:13 -0500

    [msbuild] Delay codesigning of App Extensions
    Instead of codesigning App Extensions as part of the
    App Extension build, we now codesign them as part of
    the Codesign target of the main app project.

---
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/LoadCodesignStateTaskBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.MacDev.Tasks
+{
+	public abstract class LoadCodesignStateTaskBase : Task
+	{
+		public string SessionId { get; set; }
+
+		[Required]
+		public string AppBundleDir { get; set; }
+
+		[Required]
+		public string SavedState { get; set; }
+
+		[Output]
+		public ITaskItem LoadedItem { get; set; }
+
+		public override bool Execute ()
+		{
+			Log.LogTaskName ("LoadCodesignState");
+			Log.LogTaskProperty ("AppBundleDir", AppBundleDir);
+			Log.LogTaskProperty ("SavedState", SavedState);
+
+			var item = new TaskItem (AppBundleDir);
+			PDictionary plist;
+
+			try {
+				plist = PDictionary.FromFile (SavedState);
+			} catch (Exception ex) {
+				Log.LogError ("Failed to load saved state '{0}': {1}", SavedState, ex.Message);
+				return false;
+			}
+
+			foreach (var kvp in plist) {
+				string value;
+
+				if (kvp.Value.Type == PObjectType.Boolean)
+					value = ((PBoolean) kvp.Value).Value.ToString ();
+				else if (kvp.Value.Type == PObjectType.String)
+					value = ((PString) kvp.Value).Value;
+				else
+					continue;
+
+				item.SetMetadata (kvp.Key, value);
+			}
+
+			LoadedItem = item;
+
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
---
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/SaveCodesignStateTaskBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.MacDev.Tasks
+{
+	public abstract class SaveCodesignStateTaskBase : Task
+	{
+		public string SessionId { get; set; }
+
+		[Required]
+		public string CodesignAllocate { get; set; }
+
+		public bool DisableTimestamp { get; set; }
+
+		public string Entitlements { get; set; }
+
+		public string Keychain { get; set; }
+
+		public string ResourceRules { get; set; }
+
+		[Required]
+		public string SigningKey { get; set; }
+
+		public string ExtraArgs { get; set; }
+
+		[Output]
+		[Required]
+		public string SavedState { get; set; }
+
+		public override bool Execute ()
+		{
+			Log.LogTaskName ("SaveCodesignState");
+			Log.LogTaskProperty ("CodesignAllocate", CodesignAllocate);
+			Log.LogTaskProperty ("DisableTimestamp", DisableTimestamp);
+			Log.LogTaskProperty ("Entitlements", Entitlements);
+			Log.LogTaskProperty ("Keychain", Keychain);
+			Log.LogTaskProperty ("ResourceRules", ResourceRules);
+			Log.LogTaskProperty ("SigningKey", SigningKey);
+			Log.LogTaskProperty ("ExtraArgs", ExtraArgs);
+			Log.LogTaskProperty ("SavedState", SavedState);
+
+			var plist = new PDictionary ();
+
+			plist["CodesignAllocate"] = new PString (CodesignAllocate);
+			plist["DisableTimestamp"] = new PBoolean (DisableTimestamp);
+			plist["Entitlements"] = new PString (!string.IsNullOrEmpty (Entitlements) ? Path.GetFullPath (Entitlements) : string.Empty);
+			plist["ExtraArgs"] = new PString (ExtraArgs ?? string.Empty);
+			plist["Keychain"] = new PString (Keychain ?? string.Empty);
+			plist["ResourceRules"] = new PString (ResourceRules ?? string.Empty);
+			plist["SigningKey"] = new PString (SigningKey);
+
+			try {
+				plist.Save (SavedState, true);
+			} catch (Exception ex) {
+				Log.LogError ("Could not save '{0}': {1}", SavedState, ex.Message);
+				return false;
+			}
+
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
